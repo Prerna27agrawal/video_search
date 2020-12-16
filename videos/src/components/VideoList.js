d53aa2516d9e714d2dd94c72d63de6c5ae7f5141
Prerna27agrawal <agrawalprerna1999@gmail.com>
2020-12-16 10:56:47 +0530

    deaault video added

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -2,10 +2,16 @@ import React from 'react';
 import VideoItem from './VideoItem';
  
 // in place of props we have used videos so we can write videos in place of props.videos
- const VideoList = ({videos})=>{
+ const VideoList = ({videos,onVideoSelect})=>{
     //  props.videos
     const renderedList = videos.map((video)=>{
-        return <VideoItem video ={video} />
+        return(
+             <VideoItem 
+            key={video.id.videoId} 
+                video ={video} 
+                onVideoSelect={onVideoSelect}
+             />
+        );
     });
      return <div className="ui relaxed divided list">{renderedList}</div>
  };
